69ea25c724862f627ceacfc1b4e5458719795dad
Igor Tokarev <TigorC@gmail.com>
2016-11-29 14:43:51 +0500

    Add support for @online/@here mentions.
    Fixes #2183

--- zerver/lib/bugdown/__init__.py
+++ zerver/lib/bugdown/__init__.py
@@ -996,6 +996,7 @@ class UserMentionPattern(markdown.inlinepatterns.Pattern):
 
             if wildcard:
                 current_message.mentions_wildcard = True
+                current_message.mentions_online = mention.user_mention_matches_online(name)
                 email = '*'
                 user_id = "*"
             elif user:
